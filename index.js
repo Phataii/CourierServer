b77002f74b12fbbf83de537850fc71cf4c3a09dd
phataii <esanni5@gmail.com>
2022-03-08 16:08:08 +0100

    another

--- index.js
+++ index.js
@@ -16,10 +16,34 @@ app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
 app.use(express.json());
 app.use(cookieParser());
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://suspicious-mcnulty-e79cee.netlify.app/"
+  );
+
+  // Request methods you wish to allow
+  //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  //res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(
   cors({
     origin: [
-      "https://suspicious-mcnulty-e79cee.netlify.app/",
+      // res.setHeader(
+      //   "Access-Control-Allow-Origin",
+      //   "https://suspicious-mcnulty-e79cee.netlify.app/"
+      // ),
       //"http://localhost:3000",
       // "https://mern-auth-template-tutorial.netlify.app",
     ],
