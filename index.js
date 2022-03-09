6d2540eeaea3890d13d4b1a0ff12de2fed84323d
phataii <esanni5@gmail.com>
2022-03-09 01:22:34 +0100

    latest

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ app.use(
     credentials: true,
   })
 );
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 // connect to mongoDB
 
 mongoose.connect(
